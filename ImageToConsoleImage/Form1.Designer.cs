2f09f1b288a1cbf276698d0f17fb409d46063d73
Yi Hao <steamedbunx@yahoo.com>
2019-04-16 13:50:42 -0400

    Change method name with capitalized first letter. Adding save file function.

--- ImageToConsoleImage/Form1.Designer.cs
+++ ImageToConsoleImage/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.panelImage = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBoxResult.Location = new System.Drawing.Point(26, 27);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(685, 608);
+            this.textBoxResult.TabIndex = 0;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(782, 95);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(240, 50);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load an image";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(782, 190);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(240, 50);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save as .ci";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // panelImage
+            // 
+            this.panelImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelImage.Location = new System.Drawing.Point(750, 315);
+            this.panelImage.Name = "panelImage";
+            this.panelImage.Size = new System.Drawing.Size(320, 320);
+            this.panelImage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1082, 659);
+            this.Controls.Add(this.panelImage);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "Form1";
+            this.Text = "ConsoleImage Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Panel panelImage;
     }
 }
 
--- ImageToConsoleImage/Form1.cs
+++ ImageToConsoleImage/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,166 @@ namespace ImageToConsoleImage
 {
     public partial class Form1 : Form
     {
+        Bitmap sourceImage;
+        List<Color> defaultColors;
+        string currentResult = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open An Image File";
+            theDialog.Filter = "Image Files|*.png;*.jpg";
+            theDialog.InitialDirectory = @"Desktop";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = theDialog.FileName;
+                    //MessageBox.Show(fileName);
+                    sourceImage = new Bitmap(fileName);
+                    panelImage.BackgroundImage = sourceImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            string currentResult = processImage(sourceImage);
+            string ciText = currentResult;
+            textBoxResult.Text = ciText.Replace("\n", "\r\n");
+
+        }
+
+        public string processImage(Bitmap img)
+        {
+            string result = "";
+            result += getDefultColor(img);
+            result += ProcessPixels(img);
+            return result;
+        }
+
+        public string getDefultColor(Bitmap img)
+        {
+            string result = "";
+            defaultColors = new List<Color>();
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    Color currentP = img.GetPixel(x, y);
+                    if (currentP.A == 0)
+                    {
+                        currentP = Color.Black;
+                    }
+
+                    if (!defaultColors.Contains(currentP))
+                    {
+                        if (currentP.ToArgb() != Color.Black.ToArgb())
+                        {
+                            defaultColors.Add(currentP);
+                        }
+                    }
+                }
+            }
+            foreach (Color c in defaultColors)
+            {
+                result += "#" + ((int)c.R).ToString("X2") + ((int)c.G).ToString("X2") + ((int)c.B).ToString("X2") + "\n";
+            }
+            result += "\n";
+            return result;
+        }
+
+        public string ProcessPixels(Bitmap img)
+        {
+            string result = "";
+            for (int y = 0; y < img.Height; y += 2)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    result += ProcessPixel(img.GetPixel(x, y), defaultColors);
+                    result += ProcessPixel(img.GetPixel(x, y + 1), defaultColors);
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
+        public char ProcessPixel(Color color, List<Color> defCs)
+        {
+            if (color.A == 0)
+            {
+                return '0';
+            }
+            if (color.ToArgb() == Color.Black.ToArgb())
+            {
+                return '0';
+            }
+
+            for (int i = 0; i < defCs.Count; i++)
+            {
+                if (color.ToArgb() == defCs[i].ToArgb())
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            return '1';
+                        case 1:
+                            return '2';
+                        case 2:
+                            return '3';
+                        case 3:
+                            return '4';
+                        case 4:
+                            return '5';
+                        case 5:
+                            return '6';
+                        case 6:
+                            return '7';
+                        case 7:
+                            return '8';
+                        case 8:
+                            return '9';
+                        case 9:
+                            return 'A';
+                        case 10:
+                            return 'B';
+                        case 11:
+                            return 'C';
+                        case 12:
+                            return 'D';
+                        case 13:
+                            return 'E';
+                        default:
+                            break;
+
+                    }
+                }
+            }
+            char result = '0';
+            return result;
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                myStream = new StreamWriter(saveFileDialog.FileName);
+
+                myStream.Write(currentResult);
+                myStream.Close();
+            }
         }
     }
 }
